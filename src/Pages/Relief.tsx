275d561163ea3181688743f718b89255174673c1
Igor5567 <igidod@gmail.com>
2024-08-31 20:40:48 +0200

    add Relief circles

--- src/Pages/Relief.tsx
+++ src/Pages/Relief.tsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
+
 interface ReliefProps {
   className?: string;
 }
 function Relief({ className }: ReliefProps) {
+  const [isActive, setIsActive] = useState([true, true, true]);
+
+  const toggleActive = (index: number) => {
+    setIsActive((prev) => {
+      const newState = [...prev];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
+
   return (
     <div
-      className={`h-screen w-full text-black flex justify-center items-center ${className}`}
+      className={`h-screen w-full bg-slate-200 flex justify-center items-center gap-8 flex-wrap ${className}`}
     >
-      <div className="w-[200px] h-[200px] rounded-full border-2 border-black" />
+      {isActive.map((active, index) => (
+        <div
+          key={index}
+          onClick={() => toggleActive(index)}
+          className={`${active ? "convex" : "concave"} w-[150px] h-[150px] rounded-full border-2`}
+        />
+      ))}
     </div>
   );
 }
-
 export default Relief;
