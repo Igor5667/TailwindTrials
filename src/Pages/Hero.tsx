733f08dbc012ee574e343549fee025eed8447920
Igor5567 <igidod@gmail.com>
2024-08-31 19:28:28 +0200

    add hero page

--- src/Pages/Hero.tsx
+++ src/Pages/Hero.tsx
@@ -4,11 +4,30 @@ interface HeroProps {
 
 function Hero({ className }: HeroProps) {
   return (
-    <div
-      className={`h-screen w-full text-white text-3xl bg-indigo-950 ${className}`}
+    <section
+      className={`h-screen w-full relative bg-gradient-to-tr from-[#1c2241] to-[#2e0469] text-white ${className} -z-20`}
     >
-      Hero
-    </div>
+      <div className="">
+        <img
+          src="heroImage.jpg"
+          alt="programmin"
+          className="absolute right-0 top-0 h-full object-cover overflow-hidden blur-[2px] opacity-40 -z-10"
+        />
+      </div>
+      <div className="flex flex-col justify-center items-center h-full text-center lg:absolute lg:mt-20 lg:left-16">
+        <h1
+          className=" font-bold tracking-tighter p-4 bg-gradient-to-r from-[#80fbfd] to-[#d718e9] bg-clip-text text-transparent
+         text-5xl md:text-7xl lg:text-8xl"
+        >
+          Hi, this is training site
+        </h1>
+        <div className="md:ps-10 text-xl">
+          <p className=" font-semibold">
+            Created by <i className="font-extralight ms-1 mt-5">IGOR DLUGON</i>
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
 
