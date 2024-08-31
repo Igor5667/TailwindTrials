ad51791c31563de0536ecdbffd5d8ca899168bc8
Igor5567 <igidod@gmail.com>
2024-08-31 21:47:21 +0200

    little fixes

--- src/Pages/Relief.tsx
+++ src/Pages/Relief.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import { SiNike } from "react-icons/si";
 
 interface ReliefProps {
   className?: string;
 }
 function Relief({ className }: ReliefProps) {
   const [isActive, setIsActive] = useState([true, true, true]);
-  const [isFormShown, setIsFormShown] = useState(true);
+  const [isFormShown, setIsFormShown] = useState(false);
   const [numberOfButtons, setNumberOfButtons] = useState(3);
 
   const toggleActive = (index: number) => {
@@ -31,7 +32,8 @@ function Relief({ className }: ReliefProps) {
       className={`h-screen w-full relative overflow-hidden bg-slate-200 flex justify-center items-center flex-wrap content-center gap-8  ${className}`}
     >
       <div className="absolute top-0 left-1/2 translate-x-[-50%] flex flex-col items-center gap-2  p-4 text-center">
-        <h1 className="font-bold text-3xl">Just CLick it</h1>
+        <SiNike size={100} className="-mb-5" />
+        <h1 className="font-extrabold text-3xl uppercase">Just Click It.</h1>
         {!isFormShown ? (
           <button
             className="submit-button-relief"
