df21ce79ccbffff547dc9ad63b3fb6fb29bf3a18
Igor5567 <igidod@gmail.com>
2024-08-31 21:18:46 +0200

    add adding more buttons

--- src/Pages/Relief.tsx
+++ src/Pages/Relief.tsx
@@ -5,6 +5,7 @@ interface ReliefProps {
 }
 function Relief({ className }: ReliefProps) {
   const [isActive, setIsActive] = useState([true, true, true]);
+  const [numberOfButtons, setNumberOfButtons] = useState(3);
 
   const toggleActive = (index: number) => {
     setIsActive((prev) => {
@@ -14,6 +15,15 @@ function Relief({ className }: ReliefProps) {
     });
   };
 
+  const addButtons = () => {
+    if (numberOfButtons <= 0) return;
+    let newArr = [];
+    for (let i = 0; i < numberOfButtons; i++) {
+      newArr.push(true);
+    }
+    setIsActive(newArr);
+  };
+
   return (
     <div
       className={`h-screen w-full bg-slate-200 flex justify-center items-center flex-wrap content-center gap-8  ${className}`}
@@ -21,10 +31,15 @@ function Relief({ className }: ReliefProps) {
       <div className="absolute top-2 left-1/2 translate-x-[-50%] flex flex-col items-center gap-2">
         How much do you wanna have circles?
         <input
+          value={numberOfButtons}
+          onChange={(e) => setNumberOfButtons(+e.target.value)}
           type="number"
           className="concave rounded-3xl p-2 bg-slate-200 focus:outline-none text-center"
         />
-        <button className="convex submit-button-relief rounded-3xl p-2 px-4 bg-slate-200 w-min text-sm">
+        <button
+          className="convex submit-button-relief rounded-3xl p-2 px-4 bg-slate-200 w-min text-sm"
+          onClick={addButtons}
+        >
           Submit
         </button>
       </div>
